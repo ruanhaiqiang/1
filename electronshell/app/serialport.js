c10817897a4c5b173c05423a55e2937e8fef319e
ruanhaiqiang <ruanhaiqiang@pset.suntec.net>
2017-07-12 16:20:17 +0800

    update serialport example
    Change-Id: Ifc6d67ad22363874be06c70e9c7781eed9ad7517

--- electronshell/app/serialport.js
+++ electronshell/app/serialport.js
@@ -1,13 +1,12 @@
 const electron = require('electron');
 
 const ipcMain = electron.ipcMain;
-
 ipcMain.on("initSerialPort", function(event, path, opt){
     // //此处初始化serialport，并保存serialport对象
-    let serialport = initSerialPort(path, opt, function(err){
-        console.log("===============receive render initSerialPort", path, opt, err);
+    initSerialPort(path, opt, function(err, serialport){
+        console.log("===============receive render initSerialPort", path, opt, err.toString());
         if(err){
-            event.sender.send("serialPortInited", err, null);
+            event.sender.send("serialPortInited", err.toString(), null);
             return;
         }
         //保存serialport
@@ -20,7 +19,7 @@ ipcMain.on("initSerialPort", function(event, path, opt){
 ipcMain.on("destorySerialPort", function(event, serialport){
     //此处close serialport对象
     destorySerialPort(serialport, function(err){
-        console.log("===============receive render serialportDestoryed", err);
+        console.log("===============receive render serialportDestoryed", err.toString());
         event.sender.send("serialportDestoryed", "---------------serialportDestoryed msg cb")
     })
     
@@ -29,31 +28,41 @@ ipcMain.on("destorySerialPort", function(event, serialport){
 ipcMain.on("destoryAllSerialPort", function(event){
     //此方法可以在main close时调用，防止有些打开的serialport没有关闭
     destoryAllSerialPort(function(err){
-        console.log("===============receive render destoryAllSerialPort", err);
+        console.log("===============receive render destoryAllSerialPort", err.toString());
         event.sender.send("allSerialPortDestoryed", "---------------allSerialPortDestoryed msg cb")
     })
 })
 
 
-var SerialPort = require("serialport").SerialPort;
+var serialport = require("serialport");
 var serialportList = [];
 
 function initSerialPort(path, opt, cb){
-    var item = new SerialPort(path, opt, cb);
-    serialportList.push(item);
-    return item;
+    var item = new serialport.SerialPort(path, opt, function(err){
+        if(err == null){
+            serialportList.push(item);
+            cb(null, item);
+        }else{
+            cb(err, null);
+        }
+
+    });
 }
 
 function destorySerialPort(serialport, cb){
+    let flag = false;
     for(let index in serialportList){
         if(serialportList[index] === serialport){
+            flag = true;
             serialport.close(function(err){
                 console.log(err);
                 cb(err);
                 serialportList.splice(item, 1);    
             });
-
         }
+    }
+    if(!flag){
+        cb("serilport is null")
     }
 }
 
@@ -61,17 +70,23 @@ export function destoryAllSerialPort(cb){
     var me = this;
     if(serialportList.length >= 0){
         var item = serialportList[0];
-        item.close(function(err){
-            if(err){
-                console.error(err);
-            }
-            serialportList.splice(item, 1);
-            if(serialportList.length >= 0){
-                me.destorySerialPort();
-            }else{
-                cb();
-            }
-        });
+        if(item != null){
+            item.close(function(err){
+                if(err){
+                    console.error(err);
+                    cb(err);
+                }
+                serialportList.splice(item, 1);
+                if(serialportList.length >= 0){
+                    me.destorySerialPort();
+                }else{
+                    cb();
+                }
+            });
+        }else{
+            cb("serialport is null")
+        }
+        
     }else{
         cb();
     }
